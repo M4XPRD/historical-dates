e37d8860f54225c5de77108144c2381ea7c53ac5
Max <ppmax@bk.ru>
2024-01-18 02:14:31 +0300

    Slider's data added

---
+++ src/utils/slidersData.ts
@@ -0,0 +1,50 @@
+const slidersData = [
+  {
+    years: { from: 2015, to: 2022 },
+    categories: {
+      movies: {
+
+      },
+      literature: {
+
+      },
+      science: {
+
+      },
+      music: {
+
+      },
+      sport: {
+
+      },
+      games: {
+
+      },
+    },
+  },
+  {
+    years: { from: 2008, to: 2015 },
+    categories: {
+      movies: {
+
+      },
+      literature: {
+
+      },
+      science: {
+
+      },
+      music: {
+
+      },
+      sport: {
+
+      },
+      games: {
+
+      },
+    },
+  },
+];
+
+export default slidersData;
