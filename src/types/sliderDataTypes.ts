73a9a734f3fbdf790a01ab01973a5bd19facc443
Max <ppmax@bk.ru>
2024-01-18 23:07:05 +0300

    Added types for slider's data

---
+++ src/types/sliderDataTypes.ts
@@ -0,0 +1,11 @@
+export type SliderData = {
+  years: number[];
+  categories: {
+    movies: string[];
+    literature: string[];
+    science: string[];
+    music: string[];
+    sport: string[];
+    theater: string[];
+  };
+};
