ab8c2e515ac52f45dd19c1efe90df3c0e3de05f0
Max <ppmax@bk.ru>
2024-01-19 00:14:44 +0300

    Slider app types united

---
+++ src/types/sliderTypes.ts
@@ -0,0 +1,30 @@
+export type SliderData = {
+  years: number[];
+  categories: {
+    movies: string[];
+    literature: string[];
+    science: string[];
+    music: string[];
+    sport: string[];
+    theater: string[];
+  };
+};
+
+export type EventsTypes = {
+  years: number[],
+  category: string[],
+};
+
+export type YearsTypes = {
+  firstYear: number,
+  lastYear: number,
+};
+
+export type SliderButtonsTypes = {
+  currentSlide: number,
+  handleNextSlide: () => void,
+  handlePreviousSlide: () => void,
+  slidersLength: number,
+  formattedCurrentSlide: string,
+  totalSliders: string,
+};
