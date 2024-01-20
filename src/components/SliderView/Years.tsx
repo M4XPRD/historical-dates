b4eb61ce505652815ab2d495c94848d6dc1d5e62
Max <ppmax@bk.ru>
2024-01-21 02:55:30 +0300

    Dots view finished

--- src/components/SliderView/Years.tsx
+++ src/components/SliderView/Years.tsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 import { YearsTypes } from '../../types/sliderTypes';
+import dotsPositionMapping from '../../utils/dotsPositionsMapping';
+import { DotProps } from '../../types/dotPositionsTypes';
 
 const Wrapper = styled.section`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  gap: 4rem;
 
   font-family: var(--font-family);
   font-size: var(--fs-l);
@@ -27,13 +28,14 @@ const YearsWrapper = styled.div`
   letter-spacing: -0.02em;
   max-height: 160px;
 
-  z-index: 10;
+  z-index: 0;
 `;
 
 const YearFrom = styled.div`
   display: flex;
   align-items: center;
   max-height: 160px;
+
   color: var(--colour-date-from);
 `;
 
@@ -41,6 +43,7 @@ const YearTo = styled.div`
   display: flex;
   align-items: center;
   max-height: 160px;
+
   color: var(--colour-date-to);
 `;
 
@@ -53,19 +56,19 @@ const EllipseWrapper = styled.div`
 `;
 
 const Ellipse = styled.div`
-  /* box-sizing: border-box; */
+  box-sizing: border-box;
 
   width: 500px;
   height: 500px;
 
-  border: 1px solid rgba(66, 86, 122, .2);
+  border: 1px solid rgba(66, 86, 122, 0.2);
   border-radius: 50%;
 `;
 
 const Line = styled.div`
   position: absolute;
   width: 100%;
-  border-bottom: 1px solid rgba(66, 86, 122, .1);
+  border-bottom: 1px solid rgba(66, 86, 122, 0.1);
   top: 50%;
   right: 0;
 `;
@@ -76,12 +79,36 @@ const DotsContainer = styled.div`
   height: 100%;
 `;
 
-const Dot = styled.div`
+const Dot = styled.div<DotProps>`
   position: absolute;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   width: 6px;
   height: 6px;
   background-color: var(--colour-main);
   border-radius: 50%;
+  cursor: pointer;
+  z-index: 100;
+  ${({ position }) => position};
+
+  &.active,
+  &:hover {
+    box-sizing: border-box;
+    width: 56px;
+    height: 56px;
+    background-color: var(--colour-background);
+    border: 1px solid var(--colour-dot);
+    ${({ transform }) => `transform: ${transform.hoverTransform};`}
+
+    &:after {
+      color: var(--colour-main);
+      font-size: var(--fs-xs);
+      content: ${({ id }) => `"${id}"`};
+    }
+  }
 `;
 
 const Years = ({ firstYear, lastYear }: YearsTypes) => (
@@ -93,12 +120,15 @@ const Years = ({ firstYear, lastYear }: YearsTypes) => (
     <EllipseWrapper>
       <Ellipse>
         <DotsContainer>
-          <Dot style={{ top: '49.4%', right: '-0.8%' }} />
-          <Dot style={{ top: '49.4%', left: '-0.8%' }} />
-          <Dot style={{ top: '3.2%', right: '69%' }} />
-          <Dot style={{ top: '3.2%', left: '69%' }} />
-          <Dot style={{ bottom: '3.2%', right: '69%' }} />
-          <Dot style={{ bottom: '3.2%', left: '69%' }} />
+          {dotsPositionMapping.map(({ id, position, transform }) => (
+            <Dot
+              key={id}
+              id={id}
+              position={position}
+              transform={transform}
+              className={id === 3 ? 'active' : ''}
+            />
+          ))}
         </DotsContainer>
         <Line />
       </Ellipse>
