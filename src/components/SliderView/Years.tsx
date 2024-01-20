c8cb0c0fcd010eb361385b548cf8223df1420e1a
Max <ppmax@bk.ru>
2024-01-20 18:10:57 +0300

    Added horizontal line to Years component

--- src/components/SliderView/Years.tsx
+++ src/components/SliderView/Years.tsx
@@ -13,23 +13,72 @@ const Wrapper = styled.section`
   font-weight: var(--fw-bold);
 
   max-height: 160px;
+`;
+
+const YearsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4rem;
+
   padding-right: 5px;
+  padding-bottom: 15px;
+
+  color: var(--colour-date-from);
+  letter-spacing: -0.02em;
+  max-height: 160px;
+
+  z-index: 10;
 `;
 
 const YearFrom = styled.div`
+  display: flex;
+  align-items: center;
+  max-height: 160px;
   color: var(--colour-date-from);
-  letter-spacing: -0.02em;
 `;
 
 const YearTo = styled.div`
+  display: flex;
+  align-items: center;
+  max-height: 160px;
   color: var(--colour-date-to);
-  letter-spacing: -0.02em;
+`;
+
+const EllipseWrapper = styled.div`
+  position: fixed;
+
+  display: flex;
+  justify-content: center;
+  width: 76%;
+`;
+
+const Ellipse = styled.div`
+  width: 500px;
+  height: 500px;
+
+  border: 1px solid rgba(66, 86, 122, .2);
+  border-radius: 50%;
+`;
+
+const Line = styled.div`
+  position: absolute;
+  width: 100%;
+  border-bottom: 1px solid rgba(66, 86, 122, .1);
+  top: 50%;
+  right: 0;
 `;
 
 const Years = ({ firstYear, lastYear }: YearsTypes) => (
   <Wrapper>
-    <YearFrom>{firstYear}</YearFrom>
-    <YearTo>{lastYear}</YearTo>
+    <YearsWrapper>
+      <YearFrom>{firstYear}</YearFrom>
+      <YearTo>{lastYear}</YearTo>
+    </YearsWrapper>
+    <EllipseWrapper>
+      <Ellipse>
+        <Line />
+      </Ellipse>
+    </EllipseWrapper>
   </Wrapper>
 );
 
