ca450407e393371ffcbf2471a2e298c247a7f4ac
Max <ppmax@bk.ru>
2024-01-21 02:54:42 +0300

    Added types for dots

---
+++ src/types/dotPositionsTypes.ts
@@ -0,0 +1,12 @@
+export type DotProps = {
+  id: number,
+  position: {
+    top?: string;
+    right?: string;
+    bottom?: string;
+    left?: string;
+  };
+  transform: {
+    hoverTransform: string;
+  };
+};
