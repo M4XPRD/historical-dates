54c476219eec23b122e585f4c057693e176a75e9
Max <ppmax@bk.ru>
2024-01-21 02:55:19 +0300

    Dots position mapping added

---
+++ src/utils/dotsPositionsMapping.ts
@@ -0,0 +1,34 @@
+const dotsPositionMapping = [
+  {
+    id: 1,
+    position: { top: '49.4%', left: '-0.8%' },
+    transform: { hoverTransform: 'translate(-50%, -50%)' },
+  },
+  {
+    id: 2,
+    position: { top: '3.2%', right: '69%' },
+    transform: { hoverTransform: 'translate(50%, -50%)' },
+  },
+  {
+    id: 3,
+    position: { top: '3.2%', left: '69%' },
+    transform: { hoverTransform: 'translate(-50%, -50%)' },
+  },
+  {
+    id: 4,
+    position: { top: '49.4%', right: '-0.8%' },
+    transform: { hoverTransform: 'translate(50%, -50%)' },
+  },
+  {
+    id: 5,
+    position: { bottom: '3.2%', left: '69%' },
+    transform: { hoverTransform: 'translate(-50%, 50%)' },
+  },
+  {
+    id: 6,
+    position: { bottom: '3.2%', right: '69%' },
+    transform: { hoverTransform: 'translate(50%, 50%)' },
+  },
+];
+
+export default dotsPositionMapping;
