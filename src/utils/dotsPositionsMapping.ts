e1f92a69cedc5a43fda9dc17cdf688eae5560126
Max <ppmax@bk.ru>
2024-01-22 17:28:44 +0300

    Dots positions fixed

--- src/utils/dotsPositionsMapping.ts
+++ src/utils/dotsPositionsMapping.ts
@@ -1,33 +1,33 @@
 const dotsPositionMapping = [
   {
     id: 1,
-    position: { top: '49.4%', left: '-0.8%' },
-    transform: { hoverTransform: 'translate(-50%, -50%)' },
-  },
-  {
-    id: 2,
-    position: { top: '3.2%', right: '69%' },
-    transform: { hoverTransform: 'translate(50%, -50%)' },
-  },
-  {
-    id: 3,
     position: { top: '3.2%', left: '69%' },
     transform: { hoverTransform: 'translate(-50%, -50%)' },
   },
   {
-    id: 4,
+    id: 2,
     position: { top: '49.4%', right: '-0.8%' },
     transform: { hoverTransform: 'translate(50%, -50%)' },
   },
   {
-    id: 5,
+    id: 3,
     position: { bottom: '3.2%', left: '69%' },
     transform: { hoverTransform: 'translate(-50%, 50%)' },
   },
   {
-    id: 6,
+    id: 4,
     position: { bottom: '3.2%', right: '69%' },
     transform: { hoverTransform: 'translate(50%, 50%)' },
+  },
+  {
+    id: 5,
+    position: { top: '49.4%', left: '-0.8%' },
+    transform: { hoverTransform: 'translate(-50%, -50%)' },
+  },
+  {
+    id: 6,
+    position: { top: '3.2%', right: '69%' },
+    transform: { hoverTransform: 'translate(50%, -50%)' },
   },
 ];
 
