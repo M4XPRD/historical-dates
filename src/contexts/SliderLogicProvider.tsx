81860ac4c8ae294fb768eb31132fd2f9358f6884
Max <ppmax@bk.ru>
2024-01-22 17:29:05 +0300

    Category ID added to Provider

--- src/contexts/SliderLogicProvider.tsx
+++ src/contexts/SliderLogicProvider.tsx
@@ -5,6 +5,7 @@ import SliderLogicContext from './SliderLogicContext';
 const SliderLogicProvider = ({ children }: ProviderProps) => {
   const [currentSlide, setCurrentSlide] = useState<number>(1);
   const [currentCategory, setCurrentCategory] = useState<string>('movies');
+  const [currentCategoryID, setCurrentCategoryID] = useState<number>(1);
 
   const handleNextSlide = () => {
     setCurrentSlide((current) => current + 1);
@@ -18,18 +19,26 @@ const SliderLogicProvider = ({ children }: ProviderProps) => {
     setCurrentCategory(newCategory);
   };
 
+  const handleNewCategoryID = (newCategoryID: number) => {
+    setCurrentCategoryID(newCategoryID);
+  };
+
   const providedData = useMemo(
     () => ({
       currentSlide,
       currentCategory,
+      currentCategoryID,
       handleNewCategory,
+      handleNewCategoryID,
       handleNextSlide,
       handlePreviousSlide,
     }),
     [
       currentSlide,
       currentCategory,
+      currentCategoryID,
       handleNewCategory,
+      handleNewCategoryID,
       handleNextSlide,
       handlePreviousSlide,
     ],
