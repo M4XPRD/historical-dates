215adcaefffe688a218b609294093a3a8f21ffca
Max <ppmax@bk.ru>
2024-01-25 01:59:10 +0300

    ScreenSize Provider created

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 import SliderLogicProvider from './contexts/SliderLogicProvider';
+import ScreenSizeProvider from './contexts/ScreenSizeProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SliderLogicProvider>
-      <App />
-    </SliderLogicProvider>
+    <ScreenSizeProvider>
+      <SliderLogicProvider>
+        <App />
+      </SliderLogicProvider>
+    </ScreenSizeProvider>
   </React.StrictMode>,
 );
