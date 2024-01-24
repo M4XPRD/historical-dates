b4d4ca0dd0fb984bf32f7bcaef89cd2083c4c899
Max <ppmax@bk.ru>
2024-01-25 02:01:05 +0300

    Starting animations work for Years component

--- src/components/Content/Years/Years.tsx
+++ src/components/Content/Years/Years.tsx
@@ -1,11 +1,55 @@
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
+import { TextPlugin } from 'gsap/all';
 import { YearsProps } from '../../../types/sliderTypes';
 import { YearsWrapper, YearFrom, YearTo } from './Years.styled';
+import useSliderLogic from '../../../hooks/useSliderLogic';
+import slidersData from '../../../utils/slidersData';
 
-const Years = ({ firstYear, lastYear }: YearsProps) => (
-  <YearsWrapper>
-    <YearFrom>{firstYear}</YearFrom>
-    <YearTo>{lastYear}</YearTo>
-  </YearsWrapper>
-);
+const Years = ({ startingYear, endingYear }: YearsProps) => {
+  gsap.registerPlugin(TextPlugin);
+  const { currentSlide } = useSliderLogic();
+  const yearsFromRef = useRef(null);
+  const yearsToRef = useRef(null);
+
+  useEffect(() => {
+    const { years } = slidersData[currentSlide - 1];
+    const [updatedYearFrom] = years;
+    const updatedYearTo = years[years.length - 1];
+
+    gsap.to(yearsFromRef.current, {
+      duration: 0.3,
+      text: String(updatedYearFrom),
+      ease: 'none',
+    });
+
+    gsap.to(yearsToRef.current, {
+      duration: 0.3,
+      text: String(updatedYearTo),
+      ease: 'none',
+    });
+  }, [currentSlide]);
+
+  return (
+    <YearsWrapper>
+      <YearFrom ref={yearsFromRef}>{startingYear}</YearFrom>
+      <YearTo ref={yearsToRef}>{endingYear}</YearTo>
+    </YearsWrapper>
+  );
+};
 
 export default Years;
+
+/*
+
+      gsap.to(dotsContainerRef.current, {
+        rotation: `+=${angleToRotate}`,
+        duration: 0.5,
+        ease: 'power2.inOut',
+        onStart: () => {
+          handleNewCategoryID(id);
+          updateDotsAngles(angleToRotate);
+        },
+      });
+
+*/
