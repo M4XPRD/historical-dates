f45887b872008451858d0d149724da97231e6898
Max <ppmax@bk.ru>
2024-01-26 00:19:09 +0300

    Fixed slides per view depending of screen resolution

--- src/components/Content/Timeline/Timeline.tsx
+++ src/components/Content/Timeline/Timeline.tsx
@@ -9,25 +9,30 @@ import {
   CurrentYear,
   Paragraph,
 } from './Timeline.styled';
+import useScreenSize from '../../../hooks/useScreenSize';
 
-const Timeline = ({ years, category }: TimelineProps) => (
-  <Wrapper>
-    <Swiper
-      modules={[Navigation]}
-      spaceBetween={10}
-      slidesPerView={3}
-      navigation
-    >
-      {years.map((year: number, index: number) => (
-        <SwiperSlide key={`${years}_${years.indexOf(year)}`}>
-          <EventWrapper key={years.indexOf(year)}>
-            <CurrentYear key={year}>{year}</CurrentYear>
-            <Paragraph key={category[index]}>{category[index]}</Paragraph>
-          </EventWrapper>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  </Wrapper>
-);
+const Timeline = ({ years, category }: TimelineProps) => {
+  const { screenSize } = useScreenSize();
+
+  return (
+    <Wrapper>
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={10}
+        slidesPerView={screenSize.width < 900 ? 2 : 3}
+        navigation
+      >
+        {years.map((year: number, index: number) => (
+          <SwiperSlide key={`${years}_${years.indexOf(year)}`}>
+            <EventWrapper key={years.indexOf(year)}>
+              <CurrentYear key={year}>{year}</CurrentYear>
+              <Paragraph key={category[index]}>{category[index]}</Paragraph>
+            </EventWrapper>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Wrapper>
+  );
+};
 
 export default Timeline;
