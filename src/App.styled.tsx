33f45a75546c331061e11cec5586abd85a7d3ea7
Max <ppmax@bk.ru>
2024-01-25 23:02:43 +0300

    App styles are moved to another file

---
+++ src/App.styled.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    position: relative;
+
+    display: flex;
+    align-items: baseline;
+    justify-content: center;
+
+    background-color: var(--colour-background);
+    height: 100svh;
+
+    user-select: none;
+  `;
+
+export default Wrapper;
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-import styled from 'styled-components';
 import Frame from './components/Frame/Frame';
 import AppContent from './components/Content/AppContent/AppContent';
-
-const Wrapper = styled.div`
-    position: relative;
-
-    display: flex;
-    align-items: baseline;
-    justify-content: center;
-
-    background-color: var(--colour-background);
-    height: 100svh;
-
-    user-select: none;
-  `;
+import Wrapper from './App.styled';
 
 const App = () => (
   <Wrapper>
