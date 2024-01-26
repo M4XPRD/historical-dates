0024ab85ed80134464cab7baaa5377e22ac27f47
Max <ppmax@bk.ru>
2024-01-26 06:49:20 +0300

    .

--- src/components/Content/MobileCategoryTitle/MobileCategoryTitle.tsx
+++
@@ -1,9 +0,0 @@
-import { MobileCategoryTitleProps } from '../../../types/sliderTypes';
-import { categoriesNameMapping } from '../../../utils/slidersData';
-import Div from './MobileCategoryTitle.styled';
-
-const MobileCategoryTitle = ({ currentCategoryID }: MobileCategoryTitleProps) => (
-  <Div>{categoriesNameMapping[currentCategoryID]}</Div>
-);
-
-export default MobileCategoryTitle;
--- src/components/Frames/MobileFrame.tsx
+++
@@ -1,7 +0,0 @@
-import Wrapper from './MobileFrame.styled';
-
-const MobileFrame = () => (
-  <Wrapper />
-);
-
-export default MobileFrame;
